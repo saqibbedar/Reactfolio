c2c256c1694adf866a8335160c3e4e8c338a5e71
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-08 00:13:04 +0500

    Update assests.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -31,4 +31,12 @@ const icons = [
 const textLogo = "Brand Name";
 const imgLogo = null;
 
-export { icons, textLogo, imgLogo };
+const authorDetails = {
+  authorProfile:
+    "https://media.contra.com/image/upload/c_fill,f_avif,h_176,q_auto:good,w_176/jvgmbua0vfvwokvjgnge",
+  authorDescription:
+    "I'm Ahmed, an Android Developer with over three years of specialized experience in Kotlin programming. I offer high-quality and efficient app development solutions tailored to your unique needs. Let's create user-friendly apps with clean code for your business success.",
+  getInTouchUrl: "https://contra.com/ahmed_fayaz_n6f1ycoi",
+};
+
+export { icons, textLogo, imgLogo, authorDetails };
