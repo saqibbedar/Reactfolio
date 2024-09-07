9ee5df8aee756828e6031e2bf3d8fd36720c0b05
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-08 01:56:38 +0500

    extended responsive screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'bedar-sc1': "1167px",
+        'bedar-sc2': "967px",
+      }
+    },
   },
   plugins: [],
 }
