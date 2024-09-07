6d1cefce347c752fab8b93a01814ab54557d71f4
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-08 00:14:30 +0500

    Update Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,18 +3,19 @@ import { imgLogo, textLogo } from "../../assets/assets";
 import { useState } from "react";
 
 const Navbar = () => {
-  const [activeElem, setActiveElem] = useState("Home");
+  const [activeElem, setActiveElem] = useState("About");
 
   return (
-    <div className="flex items-center justify-between px-20 border-b border-zinc-200">
+    <div className="flex items-center justify-between px-10 border mx-10 my-5 rounded-full border-zinc-200">
       {imgLogo ? (
         <img src={imgLogo} />
       ) : (
-        <Link to={"/"} className="text-3xl font-bold py-5 select-none">{textLogo}</Link>
+        <Link to={"/"} className="text-3xl font-bold py-5 select-none">
+          {textLogo}
+        </Link>
       )}
       <div className="flex gap-2">
         {[
-          "Home",
           "About",
           "Education",
           "Skills",
@@ -25,8 +26,8 @@ const Navbar = () => {
           <Link
             to={`#${elem}`}
             onClick={() => setActiveElem(elem)}
-            className={`text-xl font-semibold ${
-              activeElem === elem && "bg-black text-white"
+            className={`text-[1rem] font-[500] ${
+              activeElem === elem && "bg-[#373f51] text-white"
             } py-2 px-4 rounded-full transition-all ease-in`}
             key={1}
           >
