03a1cf006f0ddd41b5979e89c5b0eae44c581aeb
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-08 00:18:10 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Navbar, Footer } from "./components/components.js"
+import About from "./pages/About.jsx"
 
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+        <About/>
       <Footer/>
     </>
   )
