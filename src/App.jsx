94b4e0342561cb7bc0da33952e9d672e87f52db7
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-09 02:03:14 +0500

    Update App.jsx & load pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Navbar, Footer } from "./components/components.js";
-import { About, Education } from "./pages/pages.js";
+import { About, Education, Skills, Services, Projects, Contact } from "./pages/pages.js";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <div className="relative max-w-[1800px] w-full m-auto px-20 overflow-hidden">
+      <div className="relative max-w-[1800px] mt-[5rem] bedar-sc2:mt-[6.8rem] w-full m-auto px-5 bedar-sc1:px-20 overflow-auto">
         <About />
         <Education />
+        <Skills />
+        <Projects />
+        <Services />
+        <Contact />
       </div>
       <Footer />
     </>
