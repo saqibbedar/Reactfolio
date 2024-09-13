f73e56ac59a2b1c91ced0401fce74afc02592585
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-14 02:14:47 +0500

    append certificate component

--- src/pages/Certificates.jsx
+++ src/pages/Certificates.jsx
@@ -1,13 +1,30 @@
-import { PageTitle } from "../components/components.js"
-import { containerStyle } from "./styles.js"
-
+import { Certificate, PageTitle } from "../components/components.js";
+import { containerStyle } from "./styles.js";
+import { CertificatesPage } from "../assets/assets.js";
 
 const Certificates = () => {
   return (
     <div className={`${containerStyle}`}>
       <PageTitle title={"Certificates"} />
+      <div>
+        <div className="flex gap-3 flex-nowrap overflow-x-auto overflow-y-hidden custom-scrollbar">
+          {CertificatesPage.map(
+            ({ img, title, description, issuedBy, credentialURL }) => (
+              <Certificate
+                key={1}
+                isFlexShrink={true}
+                img={img}
+                title={title}
+                description={description}
+                issuedBy={issuedBy}
+                credentialURL={credentialURL}
+              />
+            )
+          )}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Certificates
+export default Certificates;
