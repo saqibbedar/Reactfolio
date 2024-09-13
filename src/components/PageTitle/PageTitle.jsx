d4979f6f3dc692743931890d7ae2a77c0f4b07e9
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-13 16:22:10 +0500

    added PageTitle component

---
+++ src/components/PageTitle/PageTitle.jsx
@@ -0,0 +1,10 @@
+const PageTitle = ({title}) => {
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      <div className="text-2xl font-medium">{title}</div>
+      <hr className="h-[1px] w-full bg-zinc-200 border-none" />
+    </div>
+  );
+};
+
+export default PageTitle;
--- src/components/components.js
+++ src/components/components.js
@@ -1,9 +1,6 @@
 import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
 import Project from "./Project/Project";
+import PageTitle from "./PageTitle/PageTitle";
 
-export {
-    Footer,
-    Navbar,
-    Project
-};
+export { Footer, Navbar, Project, PageTitle };
