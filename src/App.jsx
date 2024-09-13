ad66301b13cd15553d2da9d3577e46561636392f
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-13 16:23:54 +0500

    Update selection colors & append certificates page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { Navbar, Footer } from "./components/components.js";
-import { About, Education, Skills, Services, Projects, Contact } from "./pages/pages.js";
+import { About, Education, Skills, Services, Projects, Contact, Certificates } from "./pages/pages.js";
 
 const App = () => {
   return (
-    <>
+    <div className=" selection:bg-[#fedf89] selection:text-[#373f51]">
       <Navbar />
       <div className="relative max-w-[1800px] mt-[5rem] bedar-sc2:mt-[6.8rem] w-full m-auto px-5 bedar-sc1:px-20 overflow-auto">
         <About />
         <Education />
         <Skills />
+        <Certificates />
         <Projects />
         <Services />
         <Contact />
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
