d4b7d75082f37734ee0a5f604f737240dc50620c
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-14 02:12:23 +0500

    Update

--- src/components/components.js
+++ src/components/components.js
@@ -2,5 +2,6 @@ import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
 import Project from "./Project/Project";
 import PageTitle from "./PageTitle/PageTitle";
+import Certificate from "./Certificate/Certificate";
 
-export { Footer, Navbar, Project, PageTitle };
+export { Footer, Navbar, Project, PageTitle, Certificate };
