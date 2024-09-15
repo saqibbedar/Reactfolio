e50c579b5f2a34093d74824ba38955c8370ee36a
saqibbedar <chandio.saqib.m@gmail.com>
2024-09-16 01:36:29 +0500

    chore: add comment for base value

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Reactfolio/",
+  base: "/Reactfolio/", // replace base value with your repository name i.e. https://github.com/saqibbedar/Reactfolio so in github url repository name is "/Reactfolio/".
 });
